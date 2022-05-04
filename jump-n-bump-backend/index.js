5eb662cfa75d5e9ac90a0d166aee3e60ff85480e
a01752785 <a01752785@itesm.mx>
2022-05-03 22:43:41 -0500

    Add backend folder

---
+++ jump-n-bump-backend/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("./models");
+
+const path = require("path");
+
+// Swagger documentation
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerSpec = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Jump N Bump API",
+            version: "1.0.0"
+        }
+    },
+    apis: [`${path.join(__dirname, "./routes/*")}`]
+};
+// Set up express app
+const app = express();
+
+app.use(express.static("public"));
+
+// Set up database connection
+db.sequelize.sync();
+
+// JSON parser middleware
+app.use(bodyParser.json());
+
+// Initialize routes
+app.use("/api", require("./routes/api.routes"));
+
+// Error handling middleware
+app.use(function(err, req, res, next){
+    if (typeof err == "string")
+        res.status(422).send(err);
+    else{
+        var errorStr = err.name;
+        if (err.name == "SequelizeForeignKeyConstraintError")
+            errorStr = errorStr + ":" + err.index;
+        res.status(422).send(errorStr);
+    }
+});
+
+app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(swaggerSpec)));
+
+// Listen for requests
+app.listen(process.env.port || 80, function() {
+    console.log("Now listening for requests");
+});
---
+++ jump-n-bump-backend/models/index.js
@@ -0,0 +1,17 @@
+const dbConfig = require("../config/db.config.js");
+const Sequelize = require("sequelize");
+
+// Connect to SQL Server
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect
+});
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+db.jugador = require("./jugador.model.js")(sequelize, Sequelize);
+db.attempt = require("./attempt.model.js")(sequelize, Sequelize);
+db.playerCollectible = require("./player.collectible.model")(sequelize, Sequelize);
+
+module.exports = db;
